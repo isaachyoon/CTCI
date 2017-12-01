388d24acde65444039d4a13cfe2cefdecfd37f8d
Isaac Yoon <ysaacioon@yahoo.com>
2017-11-30 23:32:29 -0800

    4.5

--- ch4/4.5.js
+++ ch4/4.5.js
@@ -1,7 +1,23 @@
+
+/*
+Check if the following tree is a valid Binary Search Tree
+
+the following is not BST because '6' should have gone right, not left.
+send in minimum and maximum to the left and right that will determine the range.
+
+								5
+		 					 / \
+(-inf, 5)			3   7  (5, +inf)
+ 		 				 / \
+(-inf, 3)		1   6 (3, 5) 6 doesn't fit in to this range so we return false;
+
+
+*/
+
 function validateBST(node, min, max) {
 	min = min || Number.NEGATIVE_INFINITY;
 	max = max || Number.POSITIVE_INFINITY;
-	//if node doesn't exist, true;
+	//if node doesn't exist, return true because nothing wrong here;
 	if(!node) {
 		return true
 	}
@@ -11,6 +27,10 @@ function validateBST(node, min, max) {
 	//call validateBST with min, max
 	let left = validateBST(node.left, min, node.val);
 	let right = validateBST(node.right, node.val, max);
+
+	//declaring variable here uses postOrder traversal.
+	//if either left or right returned false, we are just going to return false;
+	//if the number doesn't fit between nodes, we are going to return false;
 	if(!left || !right) {
 		return false;
 	} else if (min < node.val && node.val < max) {
@@ -35,14 +55,3 @@ newTree.right.left = new tree(6)
 newTree.right.right = new tree(8)
 
 console.log(validateBST(newTree))
-/*
-the following is not BST because '6' should have gone right, not left.
-		5
-	 / \
-	3   7
- / \
-1   6
-
-send in minimum and maximum to the left and right that will determine the range.
-
-*/
