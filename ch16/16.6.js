7a3c99430d75e0d54b10e0a681647303bdd4d6d3
Isaac Yoon <ysaacioon@yahoo.com>
2018-07-09 10:43:05 -0700

    16.6

--- ch16/16.6.js
+++ ch16/16.6.js
@@ -1,64 +1,51 @@
-function smallestDifference(arr1, arr2){
-	let num1;
-	let num2;
-	let lowest = Number.POSITIVE_INFINITY;
-	for(var i = 0; i < arr1.length; i++) {
-		for(var j = 0; j < arr2.length; j++) {
-			let difference = Math.abs(arr1[i] - arr2[j]);
-			if(difference < lowest) {
-				lowest = difference;
-				num1 = arr1[i];
-				num2 = arr2[j];
-			}
+/*
+Given two arrays of integers, 
+compute the pair of values (one value in each array) with the smallest (non-negative) difference.
+Return the difference
+*/
+
+/*
+	[1, 3, 15, 11, 2]
+	[23, 127, 235, 19, 8]
+
+	[1, 2, 3, 11, 15] [8, 19, 23, 127, 235]
+	          ^        ^
+
+	                 
+	               4 vs 3
+	1. use 2 for loops and iteratue to ditermine the smallest difference
+	2.
+*/
+
+function findSmallestDifference(arr1, arr2) {
+	arr1.sort((x,y) => x - y);
+	arr2.sort((x,y) => x - y);
+	let point1 = arr1.length - 1;
+	let point2 = 0;
+	let point3 = arr1.length + arr2.length;
+	let min = Math.abs(arr1[point1] - arr2[0]);
+
+	let difference1 = Math.NEGATIVE_INFINITY;
+	let difference2 = Math.NEGATIVE_INFINITY;
+	while(point3 > 0) {
+		difference1 = Math.abs(arr1[point1] - arr2[point2 + 1]);
+		difference2 = Math.abs(arr1[point1 - 1] - arr2[point2]);
+		console.log(difference1, difference2)
+		if(difference1 < difference2 && min > difference1) {
+			min = difference1;
+			point2++;
+		} else if (difference2 < difference1 && min > difference2) {
+			min = difference2;
+			point1--;
+		} else {
+			return[arr1[point1], arr2[point2]]
+			break;
 		}
-	}
-	return [num1, num2];
+		point3--;
+	} 
+	return min;
 }
 
 let arr1 = [1, 3, 15, 11, 2];
 let arr2 = [23, 127, 235, 19, 8];
-let output = smallestDifference(arr1, arr2);
-console.log(output)
-
-
-//HOW CAN I OPTIMIZE THIS?
-/*
- sort array =
- [1, 2, 3, 11, 15]
-        ^
- [8, 19, 23, 127, 235]
-  ^
-
-*/
-
-function smallestDifferenceOPT(arr1, arr2) {
-	arr1.sort((x,y) => x - y);
-	arr2.sort((x,y) => x - y);
-
-	let point1 = 0;
-	let point2 = 0;
-
-	let lowest = Math.abs(arr1[point1] - arr2[point2]);
-	let num1;
-	let num2;
-	while(point1 < arr1.length && point2 < arr2.length) {
-		if(Math.abs(arr1[point1+1] - arr2[point2]) < lowest) {
-			lowest = Math.abs(arr1[point1+1] - arr2[point2]);
-			num1 = arr1[point1 + 1];
-			num2 = arr2[point2];
-			point1++;
-		} else if(Math.abs(arr1[point1] - arr2[point2+1]) < lowest) {
-			lowest = Math.abs(arr1[point1] - arr2[point2+1])
-			num1 = arr1[point1];
-			num2 = arr2[point2+1];
-			point2++;
-		} else {
-			point1++;
-			point2++;
-		}
-	}
-	return [num1, num2]
-}
-
-let output2 = smallestDifferenceOPT(arr1, arr2)
-console.log(output2)
+console.log(findSmallestDifference(arr1, arr2))
